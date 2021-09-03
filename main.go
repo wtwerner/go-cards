16dcbcb0ae48623939029be0f5ddf2ea4f43dff8
Tommy Werner <tommy.werner@auth0.com>
2021-09-03 17:05:43 -0500

    Create function to save deck to a file

--- main.go
+++ main.go
@@ -1,14 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
 	cards := newDeck()
 
-	hand, remainingCards := deal(cards, 5)
-
-	fmt.Println("Hand:")
-	hand.print()
-	fmt.Println("Deck:")
-	remainingCards.print()
+	cards.saveToFile("my_cards")
 }
